ce7cd782081b3d2aa175dc005a85f56487dbc10a
Maksud8117 <61826793+Maksud8117@users.noreply.github.com>
2021-06-25 15:38:19 +0500

    Add files via upload

---
+++ src/main/java/com/website/sudex/SudexApplication.java
@@ -0,0 +1,13 @@
+package com.website.sudex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudexApplication.class, args);
+    }
+
+}
